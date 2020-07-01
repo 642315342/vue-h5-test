d35d12474d597a9851f34887a0fd9adc5303a252
lpz <642315342@qq.com>
2020-07-01 19:15:46 +0800

    fix: 使用toRefs对mouseDemo进行改写

--- src/views/vue3Mouse/index.vue
+++ src/views/vue3Mouse/index.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-    {{mousePosition.x}}-{{mousePosition.y}}
+    <!-- 使用 toRefs 才能解构直接使用 -->
+    {{x}}-{{y}}
+    <!-- {{mousePosition.x}}-{{mousePosition.y}} -->
   </div>
 </template>
 <script>
+import { toRefs } from '@vue/composition-api'
 import useMousePosition from './mouse'
 
 export default {
   setup() {
     const { mousePosition } = useMousePosition()
-    // 不能进行结构， 会丢失响应式
+    // 不能进行结构， 会丢失响应式 需要使用 toRefs
     return {
-      mousePosition,
+      ...toRefs(mousePosition),
     }
   },
 }
